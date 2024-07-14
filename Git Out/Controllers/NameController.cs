72efe8c5dee3f59add3b64bfee38d4637543a57a
Taka <tk512003@gmail.com>
2024-07-14 14:07:44 -0400

    Initial commmit

---
+++ Git Out/Controllers/NameController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NameApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { name = "Your Name" });
+        }
+    }
+}
